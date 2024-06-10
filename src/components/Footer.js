b564ef6955bf58d282bb51af94b935f5749a5987
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-06-10 17:01:08 +1000

    Hide social media icons and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import SocialMediaLinks from '../components/SocialMediaLinks';
+//import SocialMediaLinks from '../components/SocialMediaLinks';
 import './Footer.css'; // Add styling as needed
 
 const Footer = () => {
   return (
     <footer>
-      <SocialMediaLinks /> {/* Render SocialMediaLinks component for social media icons */}
+      {/* <SocialMediaLinks />  Render SocialMediaLinks component for social media icons */}
 
       <div className="footer-links">
         <Link to="/terms">Terms and Conditions</Link>
