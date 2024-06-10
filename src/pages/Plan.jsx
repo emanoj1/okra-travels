bba880f4e6668d86a468b24a8647f2109cbd6139
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-06-10 16:53:38 +1000

    Embed Tally widget for Plan

--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../components/Page.css'; // Shared page styling
 
 const Plan = () => {
+    useEffect(() => {
+      // Load all embeds on the page
+      if (window.Tally) {
+        window.Tally.loadEmbeds();
+      }
+    }, []);
+      
   return (
     <div className="page">
       <h1>Let's understand your upcoming trip to India!</h1>
       <p>Once you have submitted all the details, I shall come back to you!</p>
+      <iframe 
+        data-tally-src="https://tally.so/embed/3xQ0dy?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" 
+        loading="lazy" width="100%" height="1427" frameborder="0" marginheight="0" marginwidth="0" title="Okra Travel">
+    </iframe>
     </div>
   );
 };
