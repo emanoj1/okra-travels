c0b483c14a2abbbc254ce4c651266c14df0d893f
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-06-10 12:31:33 +1000

    Update homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,29 +6,33 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home-image">
-        <img src="homepage_image_unsplash.jpg" alt="Home office" />
+        <img src="mysorepalace-ashimdsilva-unsplash.jpg" alt="Home office" />
         <p>Photo by <a href="https://unsplash.com/photos/turned-off-laptop-computer-with-near-wall-mY3_bvR74fI" target="_blank" rel="noopener noreferrer">Dillon Shook on Unsplash</a>
         </p>
       </div>
       <div className="page">
-        <h1>Welcome to the Homepage</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type 
-            specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like 
-            Aldus PageMaker including versions of Lorem Ipsum.
+      <h1>Welcome to Okra Travels</h1>
+        <p>
+          Planning a trip to India has never been easier with Okra Travels, a personalized, home-based initiative dedicated to making your travel planning seamless and enjoyable. 
         </p>
-        <h1>Where does it come from?</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        <h1>Our Services</h1>
+        <p>
+          <ul>
+            <li>Flight Research: We find the best flights to suit your schedule and budget.</li>
+            <li>Hotel Bookings: Assistance in selecting and booking the best accommodations.</li>
+            <li>Airport Transfers: Arrangements for reliable and comfortable airport transfers.</li>
+            <li>Travel Advice: General travel tips and advice to enhance your travel experience.</li>
+          </ul>
         </p>
-        <h1>Why do we use it?</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into 
-            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        
+        <h1>How It Works</h1>
+        <p>
+        <ol>
+            <li>Submit Trip Details: Provide us with the essential details of your upcoming trip.</li>
+            <li>Receive Personalized Information: We will prepare the best possible information for you to make the bookings yourself.</li>
+            <li>Airport Transfers: Arrangements for reliable and comfortable airport transfers.</li>
+            <li>Donate If You Like: If you find our service helpful, you can choose to make a donation. If not, that's perfectly fine too!</li>
+          </ol>
         </p>
       </div>
     </div>
